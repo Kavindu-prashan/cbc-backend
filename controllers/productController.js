929e09599da3f38e0aea473c79962ee0bd6c8dc4
Kavindu-Prashan <kavinduprashan7@gmail.com>
2024-12-02 09:04:42 +0530

    implement create product function🔔

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,2 +1,38 @@
-import products from '../models/product/js'
+import Product from '../models/product/js'
 
+import { isAdmin } from './userController.js'
+
+if (isAdmin(req)) {
+    res.json({
+        message:"Please login as adminto add Product"
+    })
+    return
+}
+
+
+
+export function createProduct(req,res) {
+
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    
+
+
+    product.save().then(
+        ()=>{
+            res.json({
+                message:"Product Created"
+            })
+        }
+        ).catch((error)=>{
+            res.json({
+                message:"Product not Created",
+                message:error
+            })
+        }
+        
+        )
+    
+}
