62a00a59b5329171c325a612a049e164a38842b3
Kavindu-Prashan <kavinduprashan7@gmail.com>
2025-01-16 10:52:14 +0530

    add corse into index

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv"
 import productRouter from "./routes/productRouter.js";
 import orderRouter from "./routes/orderRouter.js";
+import cors from "cors"
 
 dotenv.config()
 
 const app = express();
 const mongoUrl = process.env.MONGO_DB_URL
+
+app.use(cors)
 
 // Connect to MongoDB
 mongoose
